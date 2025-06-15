68ed4f6742a56a38533c742c9e3b281c48e2e566
Saud-454 <alfalqi1@outlook.com>
2025-06-15 07:45:05 +0300

    Add files via upload

---
+++ adad.js
@@ -0,0 +1,18 @@
+// console.warn("بداية العداد");
+
+// var divs = document.getElementsByTagName("div")
+
+// for (i = 1; i <= divs.length;i++){
+
+//     console.log("العدد الآن : " + i)
+
+// }
+// console.log("%c المجموع: ","background-color:blue;font-size:10px;color:white;", + i)
+
+// var objects = document.getElementsByTagName("div")
+
+// for (i = 0 ; i < objects.length ; i++ ){
+    
+//     objects[i].classList = " animate__animated animate__shakeY"
+//     console.log("done")
+// }
