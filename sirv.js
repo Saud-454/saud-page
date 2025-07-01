58de6fd73c0ff4d9973f2707f3fe59f543a17961
Saud-454 <alfalqi1@outlook.com>
2025-07-01 03:12:54 +0300

    Add files via upload

--- sirv.js
+++ sirv.js
@@ -6,15 +6,18 @@ document.getElementById("sirv").onclick = function(){
 
     if (thgg === true ){
         let password = window.prompt(`أدخل كلمة المرور`);
+        
+        if (password === null){
+            return
+        }
+        
         if (password === "1234") {
              window.location.href= "https://alfalqi1.online/sirv.html"
         }
-        else if (password === null){
-            event.preventDefault()
-        }
-        else (
+        
+        else {
             alert("كلمة المرور خاطئة")
-        )
+    }
     }else {
         event.preventDefault()
     }
