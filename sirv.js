3c525b34d88ada2d6821c29d38a7af48e0c7cc5e
Saud-454 <alfalqi1@outlook.com>
2025-07-25 18:45:18 +0300

    Update sirv.js

--- sirv.js
+++ sirv.js
@@ -1,16 +1,11 @@
-document.getElementById("sirv").onclick = function(){
-
+document.getElementById("sirv").onclick = function(event) {
 
     let thgg = window.confirm(`ستنتقل الى موقعي الشخصي`);
 
-    if (thgg === true ){
-window.location.href= "https://binshiekhin.github.io/crmxi/"
-        };
-else {
-        event.preventDefault()
+    if (thgg === true) {
+        window.location.href = "https://binshiekhin.github.io/crmxi/";
+    } else {
+        event.preventDefault();
     }
 
-
-}
-
-
+};
